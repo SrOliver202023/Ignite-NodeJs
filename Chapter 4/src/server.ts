8b8a39a79026e8d9a74d33a08e455b2b00509c02
SrOliver202023 <SrOliver202023@gmail.com>
2021-11-30 18:37:25 -0500

    user seed (admin)

--- Chapter 4/src/server.ts
+++ Chapter 4/src/server.ts
@@ -1,13 +1,15 @@
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
+import createConnection from "@shared/infra/typeorm";
 import { router } from './shared/infra/http/routes';
 import swaggerFile from './swagger.json';
 
-import "@shared/infra/typeorm";
 import "./shared/container";
 import { AppError } from './shared/errors/AppError';
 
+
+createConnection();
 const app = express();
 const PORT = 3333;
 
--- Chapter 4/src/shared/infra/http/routes/cars.routes.ts
+++ Chapter 4/src/shared/infra/http/routes/cars.routes.ts
@@ -1,7 +1,14 @@
 import { Router } from 'express';
 import { createCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { ensureAdmin } from '../middlewares/ensureAdmin';
+import { ensureAuhtenticated } from '../middlewares/ensureAuthenticated';
+
 const carsRoutes = Router();
 
-carsRoutes.post('/', createCarController);
+carsRoutes.post('/',
+  // ensureAdmin,
+  // ensureAuhtenticated,
+
+  createCarController);
 
 export { carsRoutes };
